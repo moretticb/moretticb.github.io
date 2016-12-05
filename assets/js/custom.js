117fa0b61aa2ddb9c978a00941357bb18dfe0965
Caio Benatti Moretti <caiodba@gmail.com>
2016-12-05 04:01:05 -0200

    implementing doodles

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,13 @@
 var doodleDates = [
 	[{b:1,e:1,name:"newyear"}], //1
 	[{b:20,e:29,name:"carnival"}], //2
-	[{b:1,e:1,name="carnival"}], //3
+	[{b:1,e:1,name:"carnival"}], //3
 	[], //4
 	[], //5
 	[], //6
 	[], //7
 	[], //8
-	[{b:7,e:7,name="7set"},{b:9,e:9,name="donosmi"}], //9
+	[{b:7,e:7,name:"7set"},{b:9,e:9,name:"donosmi"}], //9
 	[], //10
 	[], //11
 	[{b:1,e:26,name:"xmas"},{b:27,e:31,name:"newyear"}] //12
